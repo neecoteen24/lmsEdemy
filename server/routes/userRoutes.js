07704ca2f2c6c8bd87b25807db994ad99178a468
neecoteen24 <mondalanurag216@gmail.com>
2025-07-06 22:34:42 +0530

    user ratings controllers and routes added

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getUserCourseProgress, getUserData, purchaseCourse, updateUserCourseProgress, userEnrolledCourses } from '../controllers/userController.js'
+import { addUserRating, getUserCourseProgress, getUserData, purchaseCourse, updateUserCourseProgress, userEnrolledCourses } from '../controllers/userController.js'
 
 const userRouter = express.Router()
 
@@ -9,6 +9,7 @@ userRouter.post('/purchase',purchaseCourse)
 
 userRouter.post('/update-course-progress',updateUserCourseProgress)
 userRouter.post('/get-course-progress',getUserCourseProgress)
+userRouter.post('/add-rating',addUserRating)
 
 
 export default userRouter;
